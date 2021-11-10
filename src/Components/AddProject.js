f6d8fca94f9d607cc1dc05768a7e3ff7aa38b54b
aravinthraj98 <aravinth799902@gmail.com>
2021-11-10 22:31:03 +0530

    added date picker :sparkle:

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
-import {Card, Input, Icon} from 'react-native-elements';
+import {Card, Input, Icon, Button} from 'react-native-elements';
 import {Primary, Secondary} from '../Utils/Colors';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 function AddProject() {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+  const handleConfirm = date => {
+    console.log('A date has been picked: ', new Date(String(date)).getDate());
+    hideDatePicker();
+  };
   return (
     <View style={{flex: 1, justifyContent: 'center'}}>
       <Card>
@@ -30,6 +43,18 @@ function AddProject() {
             // onChangeText={text => handleChange('companyEmail', text)}
             leftIcon={<Icon name="add" size={24} color={Secondary} />}
           />
+          <Button
+            title="Add dead Line to the project"
+            onPress={showDatePicker}
+            style={{margin: 10}}
+          />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            is24Hour={true}
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+          />
           {/* <Input
             style={{backgroundColor: Secondary, color: Primary}}
             onChangeText={text => handleChange('companyEmail', text)}
@@ -42,6 +67,8 @@ function AddProject() {
               backgroundColor: Primary,
               width: '50%',
               marginLeft: '25%',
+              marginTop: 20,
+
               borderRadius: 20,
               height: 30,
             }}>
